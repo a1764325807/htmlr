37a82d7b05c05b406a776ab55ee18d9afc08e5b2
hah <46361633+a1764325807@users.noreply.github.com>
2023-01-25 05:54:45 +0800

    23

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"demo/db"
+	"demo/handler"
 	"fmt"
 	"log"
 	"net/http"
-	"wxcloudrun-golang/db"
-	"wxcloudrun-golang/service"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+
+func StaticServer(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./"+r.URL.Path)
+}
+
 func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
+
+	router := mux.NewRouter()
+	//当前端访问：http://127.0.0.1:8000/ 地址时返回当前目录下的 index.html
+	router.HandleFunc("/", StaticServer)
+	//当前端访问路径前缀为 /static/js/ 时返回访问地址指向的js文件内容
+	//当网页解析到 <script defer="defer" src="/static/js/main.c2c1cea9.js"></script> 时会执行
+	router.PathPrefix("/static/js/").HandlerFunc(StaticServer)
+	//当前端访问路径前缀为 /static/css/ 时返回访问地址指向的css文件内容
+	router.PathPrefix("/static/css/").HandlerFunc(StaticServer)
+
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
 
-	http.HandleFunc("/", service.IndexHandler)
-	http.HandleFunc("/js/", service.IndexHandlerjs)
-	http.HandleFunc("/api/count", service.CounterHandler)
-
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(srv.ListenAndServe())
 }
